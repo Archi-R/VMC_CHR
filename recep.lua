60645a91730f2407e4efcc014a0e64ef1c4030fb
hypo368 <114984095+hypo368@users.noreply.github.com>
2023-01-04 19:58:31 +0100

    adding recep.lua

---
+++ recep.lua
@@ -0,0 +1,30 @@
+function recep() 
+    -- function to recept the different values from the topic "VMC". 
+    
+    -- first, we need to connect to the client
+    client = mqtt.client("Interface","mini.arsaniit.com",1883,false) 
+    client:connect("","") 
+    
+    function message_received(length,data) 
+        room,measure = pack.unpack(data) 
+        -- depending on the room value, we append the measur in the good 
+        -- table.
+        if room == 1 then
+            table.insert(t1,measure) 
+        end 
+        
+        if room ==  2 then 
+            table.insert(t2,measure) 
+        end 
+    
+        if room == 3 then 
+            table.insert(t3,measure) 
+        end 
+    end 
+    
+    -- we subscribe to the related topic and execute the previous
+    -- function if a message if received
+    client:subscribe("VMC",mqtt.QOS0,message_received) 
+    
+    end 
+    
